1efcbdc9cab43a1879041f63f1d8cec64b8445ac
Addisalem-chane <addisalemchane19@gmail.com>
2025-05-10 12:01:11 -0800

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './App.css';
+import './index.css'
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
-    <div>
-      App
+    <div className='App'>
+      <Home/>
+
       </div>
-  )
+  );
 }
 
 export default App
